733394ff130a902d2eed43d80c11d146b077f708
Ramazan Ashurbekov <ashurbekov.ram@gmail.com>
2022-06-25 15:20:53 +0300

    App version 1.0

---
+++ ClinicalRandomizer/ClinicalRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicalRandomizerApp.swift
+//  ClinicalRandomizer
+//
+//  Created by Ramazan Ashurbekov on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ClinicalRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RandomizerScreen()
+        }
+    }
+}
---
+++ ClinicalRandomizer/RandomizerScreen.swift
@@ -0,0 +1,195 @@
+//
+//  RandomizerScreen.swift
+//  ClinicalRandomizer
+//
+//  Created by Ramazan Ashurbekov on 25.06.2022.
+//
+
+import SwiftUI
+
+struct RandomizerScreen: View {
+    @AppStorage("heightFrom") private var heightFrom: Int = 155
+    @AppStorage("heightTo") private var heightTo: Int = 195
+    @AppStorage("height") private var height: Int = 175
+    
+    @AppStorage("weightFrom") private var weightFrom: Int = 60
+    @AppStorage("weightTo") private var weightTo: Int = 90
+    @AppStorage("weight") private var weight: Int = 75
+    
+    @State private var bloodPreasureTop: Int = 120
+    @State private var bloodPreasureBottom: Int = 80
+    @State private var birthYear: Int = 1990
+    
+    private var minYear: Int {
+        1900
+    }
+    
+    private var maxYear: Int {
+        Calendar.current.component(.year, from: Date())
+    }
+    
+    private var age: Int {
+        Calendar.current.component(.year, from: Date()) - birthYear
+    }
+    
+    private var massIndex: String {
+        let heightInMeters: Float = Float(height) * 0.01
+        let massIndex: Float = Float(weight) / (heightInMeters * heightInMeters)
+        return String(format: "%.2f", massIndex)
+    }
+    
+    private var bloodPreasure: (top: Int, bottom: Int) {
+        let top = Int.random(in: 120...145)
+        let bottom: Int
+        switch top {
+        case 120...135:
+            bottom = top - Int.random(in: 40...50)
+        default:
+            bottom = top - Int.random(in: 50...55)
+        }
+        return (top: top, bottom: bottom)
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    HStack {
+                        Text("Год рождения")
+                        Spacer()
+                    }
+                    Picker("Год рождения", selection: $birthYear) {
+                        ForEach(minYear...maxYear, id: \.self) {
+                            Text(String($0))
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                    .frame(height: 120)
+                }
+                Section {
+                    MedicalCalculatorField(
+                        title: "Рост",
+                        fromValue: $heightFrom,
+                        toValue: $heightTo
+                    )
+                }
+                Section {
+                    MedicalCalculatorField(
+                        title: "Вес",
+                        fromValue: $weightFrom,
+                        toValue: $weightTo
+                    )
+                }
+                Section {
+                    HStack {
+                        Text("Возраст")
+                        Spacer(minLength: 16)
+                        Text(age.description).bold()
+                    }
+                    HStack {
+                        Text("Рост")
+                        Spacer(minLength: 16)
+                        Text(height.description).bold()
+                    }
+                    HStack {
+                        Text("Вес")
+                        Spacer(minLength: 16)
+                        Text(weight.description).bold()
+                    }
+                    HStack {
+                        Text("Индекс массы тела")
+                        Spacer(minLength: 16)
+                        Text(massIndex).bold()
+                    }
+                    HStack {
+                        Text("АД")
+                        Spacer(minLength: 16)
+                        Text(bloodPreasureTop.description).bold()
+                        Text("/").bold()
+                        Text(bloodPreasureBottom.description).bold()
+                    }
+                }
+                Section {
+                    Button {
+                        height = Int.random(in: heightFrom...heightTo)
+                        weight = Int.random(in: weightFrom...weightTo)
+                        let bloodPreasure = bloodPreasure
+                        bloodPreasureTop = bloodPreasure.top
+                        bloodPreasureBottom = bloodPreasure.bottom
+                    } label: {
+                        Text("Обновить").bold()
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Диспансеризация")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Сброс") {
+                        heightFrom = 155
+                        heightTo = 195
+                        weightFrom = 60
+                        weightTo = 90
+                        birthYear = 1990
+                    }
+                }
+                ToolbarItem(placement: .keyboard) {
+                    Button("Готово") {
+                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct MedicalCalculatorField: View {
+    let title: String
+    @Binding var fromValue: Int
+    @Binding var toValue: Int
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title).bold()
+                Spacer()
+            }
+            HStack {
+                Text("От")
+                TextField("От", text: Binding(
+                    get: {
+                        fromValue <= 0 ? "" : fromValue.description
+                    }, set: { new in
+                        fromValue = Int(new) ?? 0
+                    }
+                ))
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .frame(width: 112)
+                Spacer()
+                Text("До")
+                    .textFieldStyle(.roundedBorder)
+                TextField(
+                    "До",
+                    text: Binding(
+                        get: {
+                            toValue <= 0 ? "" : toValue.description
+                        }, set: { new in
+                            toValue = Int(new) ?? 0
+                        }
+                    )
+                )
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .frame(width: 112)
+            }
+        }
+        .padding(.vertical, 8)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        RandomizerScreen()
+    }
+}
